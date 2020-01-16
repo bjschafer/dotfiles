be45ab49bb636d2749a3ff72f985de1b371832e0
Braxton Schafer <braxton@epic.com>
2020-01-16 16:11:57 -0600

    add task.bash and also fix? ssh agent stuff

---
+++ .local/bin/task.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# $1 = tty to watch
+
+hiddenTags="-Deferred -Delegated"
+
+while (true)
+do
+    clear
+    uname -n
+    date "+%T"
+    task calendar
+    echo
+    echo OVERDUE
+    task +OVERDUE -TODAY "$hiddenTags" list
+    echo
+    echo TODAY
+    task +TODAY -Deferred list
+    echo
+    echo WEEK
+    task -OVERDUE -TODAY "$hiddenTags" +WEEK list
+    echo
+    echo LATER
+    task -OVERDUE -WEEK "$hiddenTags" due.any: list
+    echo
+    echo NONE
+    task "$hiddenTags" due.none: list
+    echo
+    echo "DELEGATED: $(task count +Delegated) TASKS, task +Delegated list to view."
+    echo
+    echo "DEFERRED: $(task count +Deferred) TASKS, task +Deferred list to view."
+    inotifywait -qq -e modify "$1" "$HOME/vimwiki"
+    sleep 5
+done
--- .zshrc
+++ .zshrc
@@ -138,3 +138,10 @@ source "$ZSH/oh-my-zsh.sh"
 if [ -n "$SSH_CLIENT" ] && ! [[ "$TERM_CLIENT" == 'PuTTY' ]]; then
     export PROMPT="[%m]$PROMPT"
 fi
+
+if (( $+commands[ssh-agent] )) && [ ! -S ~/.ssh/ssh_auth_sock ]; then
+  eval `ssh-agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add
