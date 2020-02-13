3f1a5f507a1b74cc6bd18f0d992b1352fd2f155d
Braxton Schafer <braxton@epic.com>
2020-02-13 13:30:53 -0600

    set sane colors for psreadline, since it's bad and should feel bad.

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
+
+#find better colors, i dare you.
+Set-PSReadLineOption -EditMode vi -Colors @{
+	'Operator'  = [ConsoleColor]::DarkGreen
+	'Parameter' = [ConsoleColor]::DarkYellow
+}
