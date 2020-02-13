48885f96bbdde87b07e6b55830758cc378ac7698
Braxton Schafer <braxton@epic.com>
2020-02-13 13:34:01 -0600

    Make PS profile a bit more robust

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,21 @@
+If (!(Get-Module -Name 'posh-git'))
+{
+	Install-Module posh-git -noconfirm -Force
+}
+If (!(Get-Module -Name 'oh-my-posh'))
+{
+	Install-Module oh-my-posh -noconfirm -Force
+}
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 
-#find better colors, i dare you.
-Set-PSReadLineOption -EditMode vi -Colors @{
-	'Operator'  = [ConsoleColor]::DarkGreen
-	'Parameter' = [ConsoleColor]::DarkYellow
+If (Get-Module -Name 'PSReadline')
+{
+	#find better colors, i dare you.
+	Set-PSReadLineOption -EditMode vi -Colors @{
+		'Operator'  = [ConsoleColor]::DarkGreen
+		'Parameter' = [ConsoleColor]::DarkYellow
+	}
 }
