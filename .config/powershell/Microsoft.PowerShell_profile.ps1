76eb1e750b31285c870aee4ba8d1f134a3cbf800
Braxton Schafer <braxton@epic.com>
2021-03-25 10:46:42 -0500

    add starship for posh

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,13 @@
-If (!(Get-Module -Name 'posh-git'))
-{
-	Install-Module posh-git -Force
-}
-If (!(Get-Module -Name 'oh-my-posh'))
-{
-	Install-Module oh-my-posh -Force
-}
-
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme Paradox
-
-If (Get-Module -Name 'PSReadline')
+If (Get-Module -ListAvailable -Name 'PSReadline')
 {
 	#find better colors, i dare you.
 	Set-PSReadLineOption -EditMode vi -Colors @{
 		'Operator'  = [ConsoleColor]::DarkGreen
 		'Parameter' = [ConsoleColor]::DarkYellow
 	}
+}
+
+If (Get-Command starship)
+{
+        Invoke-Expression $(starship init powershell)
 }
