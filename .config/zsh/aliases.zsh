5820f54be7c77740cf44edc0c6387299ceb3265e
Braxton Schafer <braxton@epic.com>
2022-03-10 11:12:53 -0600

    split functions out from aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -30,54 +30,8 @@ fi
 
 alias purevim='vim -u NONE'
 
-OSFAMILY=$(
-    if ! command -v lsb_release >/dev/null; then
-        echo 'Unknown'
-        exit
-    fi
-    lsbrelease=$(lsb_release -a 2>/dev/null | awk '/Distributor ID/ {print $NF}')
-    if [[ $lsbrelease =~ Manjaro* ]] || [[ $lsbrelease =~ Arch* ]]; then
-        echo Arch
-    elif [[ $lsbrelease == Ubuntu ]] || [[ $lsbrelease == Debian ]] || [[ $lsbrelease == Raspbian ]]; then
-        echo Debian
-    else
-        echo "not implemented"
-    fi
-)
-export OSFAMILY
+######################################
+# global aliases (expanded anywhere) #
+######################################
 
-reboot-required() {
-    if [[ "$OSFAMILY" == "Arch" ]]; then
-        diff -q <(sed 's/ x64//g' "$(find /boot/linux* | sort -h | tail -1)") <(uname -r ) >/dev/null || echo 'yes'
-    elif [[ "$OSFAMILY" == "Debian" ]]; then
-        test -f '/var/run/reboot-required' && echo 'yes'
-    fi
-}
-
-tmux-select() {
-	local sessions
-	sessions=$(tmux list-sessions -F '#S')
-	if [ "$(echo "$sessions" | wc -l)" -eq 1 ]; then
-		tmux at -t "$sessions"
-	else
-		select sel in $(tmux list-sessions -F '#S'); do break; done && tmux at -t "$sel"
-	fi
-}
-tmux-kill() {
-	select sel in $(tmux list-sessions -F '#S'); do break; done && tmux kill-session -t "$sel"
-}
-
-ssh-fingerprint() {
-	local pubkey="$1"
-    shift
-	if [ -f "$pubkey" ]; then
-		ssh-keygen -lf "$pubkey" "$@"
-	else
-		ssh-keygen -lf <("$pubkey") "$@"
-	fi
-}
-
-clr() {
-    clear
-    echo "Currently logged into $HOST on $TTY, as $USER in directory $PWD."
-}
+alias -g @oy=' -o yaml'
