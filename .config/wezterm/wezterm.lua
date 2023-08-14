df27fd6a5137eba8c9b58f517b20a8538dd80cd3
Braxton Schafer <bschafer@cloudflare.com>
2023-08-14 14:37:43 -0500

    try to make font more bearable

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,11 +17,23 @@ config.color_scheme = 'OneHalfDark'
 
 config.enable_tab_bar = false
 
-config.font = wezterm.font("InconsolataNerdFont", {weight="Regular", stretch="Normal", style="Normal"}) -- /usr/share/fonts/OTF/Caskaydia Cove Nerd Font Complete Regular.otf, FontConfig
-config.font_size = 10.0
+config.freetype_load_flags = 'FORCE_AUTOHINT'
+local fontname
 
 config.term = "wezterm"
 config.warn_about_missing_glyphs = false
+
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+    -- macOS-specific config
+    fontname = "Inconsolata Nerd Font Mono"
+    config.font_size = 12.0
+    config.window_decorations = "RESIZE" -- remove titlebar, but keep it resizable.
+else
+    config.font_size = 9.0
+    fontname = "InconsolataNerdFont"
+end
+
+config.font = wezterm.font(fontname, {weight="Regular", stretch="Normal", style="Normal"}) -- /usr/share/fonts/OTF/Caskaydia Cove Nerd Font Complete Regular.otf, FontConfig
 
 -- and finally, return the configuration to wezterm
 return config
