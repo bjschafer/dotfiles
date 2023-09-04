b57eec0f94dc083347043d662801be4a29359011
Braxton Schafer <braxton@cmdcentral.xyz>
2023-09-03 21:54:50 -0500

    add find keymaps lol

--- .config/nvim/lua/configs/telescope-config/init.lua
+++ .config/nvim/lua/configs/telescope-config/init.lua
@@ -51,10 +51,11 @@ require('telescope').setup {
 
 -- keybindings
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymap, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
