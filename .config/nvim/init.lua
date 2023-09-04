b854341e986ce5bd0595268e404b93cba7e70a08
Braxton Schafer <braxton@cmdcentral.xyz>
2023-09-03 21:48:01 -0500

    bonus config for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,11 @@ require('options')
 require('plugins')
 require('keybindings')
 
+require('configs/bufferline-config')
 require('configs/indent-blankline-config')
-require('configs/lsp_config')
+require('configs/lsp-config')
+require('configs/lualine-config')
+require('configs/telescope-config')
 require('configs/treesitter-config')
 require('configs/whichkey-config')
 
---
+++ .config/nvim/lua/configs/bufferline-config/init.lua
@@ -0,0 +1,5 @@
+require("bufferline").setup{}
+-- vim.cmd[[
+-- nnoremap <silent>pn :BufferLineMoveNext<CR>
+-- nnoremap <silent>hn :BufferLineMovePrev<CR>
+-- ]]
