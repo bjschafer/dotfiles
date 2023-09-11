3c781774a344446e1540bdbd018adaebbad673ac
Braxton Schafer <bschafer@cloudflare.com>
2023-09-11 11:47:47 -0500

    fix(nvim): telescope keymaps error

--- .config/nvim/lua/configs/telescope-config/init.lua
+++ .config/nvim/lua/configs/telescope-config/init.lua
@@ -55,7 +55,7 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fk', builtin.keymap, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
