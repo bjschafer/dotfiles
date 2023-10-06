5b2a8ba979fce17c57d9fb6c63c7a0d49602ea5f
Braxton Schafer <bschafer@cloudflare.com>
2023-10-06 11:49:38 -0500

    feat(nvim): add whichkey mappings for normal mode

--- .config/nvim/lua/keybindings/init.lua
+++ .config/nvim/lua/keybindings/init.lua
@@ -1,9 +1,16 @@
+local wk = require("which-key")
+local opts = { prefix = "" }
+local mappings = {}
+
 vim.g.mapleader = " "
+
+vim.keymap.set("", "<F5>", ":set paste<CR>")
 
 vim.keymap.set("", "<F6>", function()
     vim.opt.relativenumber = not vim.opt.relativenumber
     vim.opt.number = not vim.opt.number
 end)
+mappings["<F6>"] = { "Toggle line numbers" }
 
 -- wrapped lines goes down/up to next row, rather than next line in file
 vim.keymap.set("", "j", "gj")
@@ -18,3 +25,7 @@ vim.keymap.set("v", ".", ":normal .<CR>") -- allow using repeat operator with vi
 vim.keymap.set("c", "w!!", "w !sudo tee % >/dev/null") -- really write the file
 
 vim.keymap.set("", "<Leader>=", "<C-w>=") -- adjust viewports to all be the same size
+
+vim.keymap.set("", "?", ":WhichKey<CR>") -- show all mappings
+
+wk.register(mappings, opts)
