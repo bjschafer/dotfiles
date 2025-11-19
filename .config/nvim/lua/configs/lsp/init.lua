cace63e8f350406c345a2975abbbb4df80ab9c14
Braxton Schafer <braxton@cmdcentral.xyz>
2025-11-18 21:11:44 -0600

    feat: add comprehensive LSP configuration with multiple language servers
    Added language servers: pyright (Python), gopls (Go), ts_ls (TypeScript/JavaScript), rust_analyzer (Rust), bashls (Shell), terraformls (Terraform), marksman (Markdown), yamlls (YAML/Kubernetes with schemas). Includes full LSP keybindings (gd, gr, K, <leader>rn, <leader>ca, etc.) and diagnostic configuration.

--- .config/nvim/lua/configs/lsp/init.lua
+++ .config/nvim/lua/configs/lsp/init.lua
@@ -1,11 +1,139 @@
+-- LSP Keybindings
+local on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+
+    -- Diagnostics
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+end
+
+-- Configure diagnostic display
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+
+-- Lua Language Server
 vim.lsp.config("luals", {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
     root_markers = { ".luarc.json", ".luarc.jsonc" },
+    on_attach = on_attach,
 })
 
-vim.lsp.enable("luals")
+-- Python
+vim.lsp.config("pyright", {
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile" },
+    on_attach = on_attach,
+})
 
+-- Go
+vim.lsp.config("gopls", {
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_markers = { "go.work", "go.mod", ".git" },
+    on_attach = on_attach,
+})
+
+-- JavaScript/TypeScript
+vim.lsp.config("ts_ls", {
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+    on_attach = on_attach,
+})
+
+-- Rust
+vim.lsp.config("rust_analyzer", {
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_markers = { "Cargo.toml", "rust-project.json" },
+    on_attach = on_attach,
+})
+
+-- Shell
+vim.lsp.config("bashls", {
+    cmd = { "bash-language-server", "start" },
+    filetypes = { "sh", "bash" },
+    root_markers = { ".git" },
+    on_attach = on_attach,
+})
+
+-- Terraform
+vim.lsp.config("terraformls", {
+    cmd = { "terraform-ls", "serve" },
+    filetypes = { "terraform", "tf", "hcl" },
+    root_markers = { ".terraform", ".git" },
+    on_attach = on_attach,
+})
+
+-- Markdown
+vim.lsp.config("marksman", {
+    cmd = { "marksman", "server" },
+    filetypes = { "markdown", "markdown.mdx" },
+    root_markers = { ".git", ".marksman.toml" },
+    on_attach = on_attach,
+})
+
+-- YAML/Kubernetes
+vim.lsp.config("yamlls", {
+    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+    root_markers = { ".git" },
+    on_attach = on_attach,
+    settings = {
+        yaml = {
+            schemas = {
+                kubernetes = "*.yaml",
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+            },
+        },
+    },
+})
+
+-- Enable configured language servers
+vim.lsp.enable("luals")
+vim.lsp.enable("pyright")
+vim.lsp.enable("gopls")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("bashls")
+vim.lsp.enable("terraformls")
+vim.lsp.enable("marksman")
+vim.lsp.enable("yamlls")
+
+-- Enable inlay hints globally
 vim.lsp.inlay_hint.enable()
 
 -- https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/after/plugin/lsp.lua#L31-L32
