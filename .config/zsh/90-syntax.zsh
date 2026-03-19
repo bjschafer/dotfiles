8766e733095278afd8bf1f447b3c36ab8c83d3e2
Braxton Schafer <bschafer@cloudflare.com>
2026-03-19 13:52:35 -0500

    feat(zsh): add you-should-use

--- .config/zsh/90-syntax.zsh
+++ .config/zsh/90-syntax.zsh
@@ -10,6 +10,20 @@ elif [[ "$system_type" == 'debian' ]]; then
 elif [[ "$system_type" == 'darwin' ]]; then
     syntax="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
     suggestions="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+    youshoulduse="$(brew --prefix)/share/zsh-you-should-use/you-should-use.plugin.zsh"
 fi
 source_if_exists "$syntax"
 source_if_exists "$suggestions"
+source_if_exists "$youshoulduse"
+
+# zsh preexec $2 is the alias-expanded command, but YSU compares $2 against
+# raw alias values. Chained aliases (e.g. l='ls -lah' + ls='eza') break
+# because $2='eza -lah' never matches 'ls -lah'. Use $1 (typed) for matching.
+if (( $+functions[_check_aliases] )); then
+    functions[_check_aliases_orig]="$functions[_check_aliases]"
+    _check_aliases() { _check_aliases_orig "$1" "$1"; }
+    functions[_check_global_aliases_orig]="$functions[_check_global_aliases]"
+    _check_global_aliases() { _check_global_aliases_orig "$1" "$1"; }
+fi
+
+YSU_MESSGAGE_POSITION='after' # at the bottom of the command
